b78bbdcbbc0f10c9ba57cf5d037a8510e835a0be
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2025-09-30 18:29:55 +0300

    remove redundant shit

--- backend/roadmaps-catalog-service/Out/SkillMap.DataSource.RoadmapSh/Configs/DescriptionsDataSourceConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using SkillMap.Shared;
-
-namespace SkillMap.DataSource.RoadmapSh.Configs;
-
-public class DescriptionsDataSourceConfig : DataSourceConfig
-{
-    public string RoadmapRootNodeId { get; set; }
-    public GitHubConfig Source { get; set; }
-}
