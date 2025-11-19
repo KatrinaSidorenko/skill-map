5ab5a8243bab30bfe59ca7c0de972733622dfaf3
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2025-11-19 08:26:14 +0200

    fix run time error

--- frontend/skill-map/src/app/[locale]/assessment-panel/[testId]/page.tsx
+++ frontend/skill-map/src/app/[locale]/assessment-panel/[testId]/page.tsx
@@ -1,5 +1,11 @@
 import { TestFormWrapper } from '@/features/assessment/test-form';
 
-export default function Page({ params }: { params: { testId: string } }) {
-  return <TestFormWrapper testId={params.testId} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ testId: string }>;
+}) {
+  const { testId } = await params;
+
+  return <TestFormWrapper testId={testId} />;
 }
