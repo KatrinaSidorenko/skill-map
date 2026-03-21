dd5bba6ff062724275570a139536418019b5d3db
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2026-03-21 09:39:11 +0200

    add workspace title copy

--- backend/core-service/SkillMap.Business/RoadmapsWorkspace/Features/CreateRoadmapWorkspace/CreateRoadmapWorkspaceHandler.cs
+++ backend/core-service/SkillMap.Business/RoadmapsWorkspace/Features/CreateRoadmapWorkspace/CreateRoadmapWorkspaceHandler.cs
@@ -1,22 +1,29 @@
 ﻿using JetBrains.Annotations;
 
+using LearningPlatform.Roadmap.Business.Contracts;
+
 using MediatR;
 
 using SkillMap.Business.Abstractions;
 using SkillMap.Business.RoadmapsWorkspace.IntegrationEvents;
 using SkillMap.Core.RoadmapsWorkspace;
 using SkillMap.Shared.EventBus;
+using SkillMap.Shared.Models;
 
 namespace SkillMap.Business.RoadmapsWorkspace.Features.CreateRoadmapWorkspace;
 
 [UsedImplicitly]
-internal sealed class CreateRoadmapWorkspaceHandler(IRepository<RoadmapWorkspace> repository, IEventBus eventBus) : IRequestHandler<CreateRoadmapWorkspaceCommand, long>
+internal sealed class CreateRoadmapWorkspaceHandler(IRepository<RoadmapWorkspace> repository, IRoadmapBlueprintRepository blueprintRepository, IEventBus eventBus) : IRequestHandler<CreateRoadmapWorkspaceCommand, long>
 {
     public async Task<long> Handle(CreateRoadmapWorkspaceCommand request, CancellationToken cancellationToken)
     {
         var roadmapBookmark = new RoadmapWorkspace(request.UserId, request.RoadmapId, null);
-        roadmapBookmark.SetMetadata(new RoadmapWorkspaceMetadata("Who knows the roadmap?", string.Empty, string.Empty));
-        // todo: go to external store to get the roadmap data
+        var blueprintResult = await blueprintRepository.GetPlainRoadmapsByIds([request.RoadmapId], DefaultParams.SearchingParams, cancellationToken);
+        if (blueprintResult.IsFailed) throw new InvalidOperationException($"Failed to get roadmap blueprint for roadmap id {request.RoadmapId}. Error: {blueprintResult.Message}");
+       
+        var blueprint = blueprintResult.Data.Result.Single();
+        roadmapBookmark.SetMetadata(new RoadmapWorkspaceMetadata(blueprint.Title, blueprint.Description, blueprint.ImageUrl));
+
         await repository.AddAsync(roadmapBookmark, cancellationToken);
         await repository.SaveChangesAsync(cancellationToken);
 
