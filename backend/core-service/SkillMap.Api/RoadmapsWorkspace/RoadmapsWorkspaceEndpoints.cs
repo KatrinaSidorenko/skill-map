2834fe7346a973ed96742096582e4807fffcae0d
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2026-04-15 13:19:53 +0300

    remove prev implementation endpoints

--- backend/core-service/SkillMap.Api/RoadmapsWorkspace/RoadmapsWorkspaceEndpoints.cs
+++ backend/core-service/SkillMap.Api/RoadmapsWorkspace/RoadmapsWorkspaceEndpoints.cs
@@ -5,11 +5,6 @@ using SkillMap.Api.RoadmapsWorkspace.CreateRoadmapWorkspace;
 using SkillMap.Api.RoadmapsWorkspace.DeleteRoadmapWorkspace;
 using SkillMap.Api.RoadmapsWorkspace.GetRoadmapWorkspaces;
 using SkillMap.Api.RoadmapsWorkspace.GetRoadmapWorkspaceSummary;
-using SkillMap.Api.RoadmapsWorkspace.WorkspaceEvents.AddLearningItem;
-using SkillMap.Api.RoadmapsWorkspace.WorkspaceEvents.CreateLearningItemConnection;
-using SkillMap.Api.RoadmapsWorkspace.WorkspaceEvents.DeleteLearningItem;
-using SkillMap.Api.RoadmapsWorkspace.WorkspaceEvents.DeleteLearningItemConnection;
-using SkillMap.Api.RoadmapsWorkspace.WorkspaceEvents.UpdateLearningItem;
 
 namespace SkillMap.Api.Roadmap;
 
@@ -22,13 +17,6 @@ internal static class RoadmapsWorkspaceEndpoints
         app.MapGetRoadmapWorkspaceSummary();
         app.MapCreateRoadmapWorkspace();
         app.MapDeleteRoadmapWorkspace();
-
-        //app.MapAddLearningItem();
-        //app.MapUpdateLearningItem();
-        //app.MapDeleteLearningItem();
-        //app.MapCreateLearningItemConnection();
-        //app.MapDeleteLearningItemConnection();
-        //app.MapGetWorkspaceEventsStatus();
 
         app.MapHub<WorkspaceHub>("api/hubs/workspace");
     }
