08d95adaf9052b46dbd2529aa104fb74441d7b93
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2026-04-25 09:50:43 +0300

    add config options

---
+++ backend/roadmap-workspace/LearningPlatform.Workspace.WebSockets/RoadmapWorkspaceActionProducerOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace LearningPlatform.Workspace.WebSockets.Contracts;
+public class RoadmapWorkspaceActionProducerOptions
+{
+    public const string SectionName = "RoadmapWorkspaceActionProducerOptions";
+    public string BootstrapServers { get; set; } = string.Empty;
+    public string TopicName { get; set; }
+}
