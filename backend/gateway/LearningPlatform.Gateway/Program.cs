98119659e702f9a1594a50632bad91734975d476
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2026-04-28 15:41:54 +0300

    refactor: add simple gateway auth

--- backend/gateway/LearningPlatform.Gateway/Program.cs
+++ backend/gateway/LearningPlatform.Gateway/Program.cs
@@ -1,10 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -14,18 +18,37 @@ builder.Services.AddCors(options =>
               .AllowCredentials());
 });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var jwtIssuer = builder.Configuration.GetSection("JwtSettings:Issuer").Get<string>();
+var jwtAudience = builder.Configuration.GetSection("JwtSettings:Audience").Get<string>();
+var jwtKey = builder.Configuration.GetSection("JwtSettings:Secret").Get<string>();
+builder.Services.AddAuthentication(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey ?? "")),
+        };
+    });
+
+var app = builder.Build();
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+// set custom request headers for authenticated users in future
 
 app.MapReverseProxy();
 
