6a11058cab5635fe76e988ba20f1037c99ccf9ec
Kateryna Sydorenko <126757452+KatrinaSidorenko@users.noreply.github.com>
2026-05-06 13:46:48 +0300

    refactor: update passwords hashing

---
+++ backend/shared/SkillMap.Shared/Options/PasswordHashOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace SkillMap.Shared.Options;
+public class PasswordHashOptions
+{
+    public const string SectionName = "PasswordHashOptions";
+
+    public int MemorySize { get; set; } 
+    public int Iterations { get; set; }
+    public int DegreeOfParallelism { get; set; }
+    public int SaltSize { get; set; }
+    public int HashSize { get; set; }
+    public string Separator { get; set; } = ":";
+}
