a83cbe73992006afdf4924eb5165a8fd9b58b1b7
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2026-05-25 10:53:48 +0300

    refactor: simplify create blueprint based workspace

--- backend/core-service/SkillMap.Business/RoadmapsWorkspace/Features/CreateRoadmapWorkspace/CreateRoadmapWorkspaceHandler.cs
+++ backend/core-service/SkillMap.Business/RoadmapsWorkspace/Features/CreateRoadmapWorkspace/CreateRoadmapWorkspaceHandler.cs
@@ -4,7 +4,10 @@ using LearningPlatform.Roadmap.Business.Contracts;
 
 using MediatR;
 
+using Microsoft.Extensions.Logging;
+
 using SkillMap.Business.Abstractions;
+using SkillMap.Business.RoadmapsWorkspace.Features.CreateWorkspaceSnapshot.Blueprint;
 using SkillMap.Business.RoadmapsWorkspace.IntegrationEvents;
 using SkillMap.Core.RoadmapsWorkspace;
 using SkillMap.Shared.EventBus;
@@ -13,7 +16,11 @@ using SkillMap.Shared.Models;
 namespace SkillMap.Business.RoadmapsWorkspace.Features.CreateRoadmapWorkspace;
 
 [UsedImplicitly]
-internal sealed class CreateRoadmapWorkspaceHandler(IRepository<RoadmapWorkspace> repository, IRoadmapBlueprintRepository blueprintRepository, IEventBus eventBus) : IRequestHandler<CreateRoadmapWorkspaceCommand, long>
+internal sealed class CreateRoadmapWorkspaceHandler(
+    IRepository<RoadmapWorkspace> repository, 
+    ILogger<CreateRoadmapWorkspaceHandler> logger,
+    IMediator mediator, 
+    IRoadmapBlueprintRepository blueprintRepository) : IRequestHandler<CreateRoadmapWorkspaceCommand, long>
 {
     public async Task<long> Handle(CreateRoadmapWorkspaceCommand request, CancellationToken cancellationToken)
     {
@@ -26,17 +33,34 @@ internal sealed class CreateRoadmapWorkspaceHandler(IRepository<RoadmapWorkspace
             return roadmapWorkspace.Id;
         }
 
-        roadmapWorkspace = new RoadmapWorkspace(request.UserId, request.RoadmapId, null);
-        var blueprintResult = await blueprintRepository.GetPlainRoadmapsByIds([request.RoadmapId], DefaultParams.SearchingParams, cancellationToken);
-        if (blueprintResult.IsFailed) throw new InvalidOperationException($"Failed to get roadmap blueprint for roadmap id {request.RoadmapId}. Error: {blueprintResult.Message}");
+        try
+        {
+            roadmapWorkspace = new RoadmapWorkspace(request.UserId, request.RoadmapId, null);
+            var blueprintResult = await blueprintRepository.GetPlainRoadmapsByIds([request.RoadmapId], DefaultParams.SearchingParams, cancellationToken);
+            if (blueprintResult.IsFailed) throw new InvalidOperationException($"Failed to get roadmap blueprint for roadmap id {request.RoadmapId}. Error: {blueprintResult.Message}");
 
-        var blueprint = blueprintResult.Data.Result.Single();
-        roadmapWorkspace.SetMetadata(blueprint.Title, blueprint.Description, blueprint.ImageUrl);
+            var blueprint = blueprintResult.Data.Result.Single();
+            roadmapWorkspace.SetMetadata(blueprint.Title, blueprint.Description, blueprint.ImageUrl);
 
-        await repository.AddAsync(roadmapWorkspace, cancellationToken);
-        await repository.SaveChangesAsync(cancellationToken);
+            await repository.AddAsync(roadmapWorkspace, cancellationToken);
+            await repository.SaveChangesAsync(cancellationToken);
 
-        await eventBus.PublishAsync(RoadmapWorkspaceCreatedEvent.Create(roadmapWorkspace.Id, roadmapWorkspace.RoadmapId, false), cancellationToken);
+            var command = new BuildBlueprintWorkspaceSnapshotCommand(roadmapWorkspace.Id, roadmapWorkspace.RoadmapId);
+            await mediator.Send(command, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            if (roadmapWorkspace is not null)
+            {
+                await repository.DeleteAsync(roadmapWorkspace.Id, cancellationToken);
+                await repository.SaveChangesAsync(cancellationToken);
+            }
+          
+            logger.LogError(ex, "An error occurred while creating roadmap workspace for user {UserId} and roadmap {RoadmapId}", request.UserId, request.RoadmapId);
+            throw;
+        }
+       
+
         return roadmapWorkspace.Id;
     }
 }
