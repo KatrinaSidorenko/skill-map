0df2f907a120cda1e9a6f7a009142d2ada94df41
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2026-06-06 16:04:08 +0300

    clean up

--- k6/src/helpers/env.ts
+++ k6/src/helpers/env.ts
@@ -2,7 +2,7 @@ export const getEnv = () => {
     const apiBase = __ENV.API_BASE || 'https://localhost:7066';
     return {
         API_BASE: apiBase,
-        MOCK_DATA_PATH: __ENV.MOCK_DATA_PATH || 'D:\\university-projects\\skill-map-tests\\load_testing\\mock_data.json',
+        MOCK_DATA_PATH: __ENV.MOCK_DATA_PATH || 'SOME_PATH\\mock_data.json',
         API_ROADMAPS_WORKSPACE: `${apiBase}/api/roadmaps-workspace`,
         WS_BASE_URL: __ENV.WS_BASE_URL || 'wss://localhost:7066',
     }
